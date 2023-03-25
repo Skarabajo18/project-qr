a22c66fde34a349cdac97d8e4499651af188dc84
Skarabajo18 <felipesegovia14@gmail.com>
2023-03-25 06:58:56 -0300

    validar-useradmin

--- myapp/views.py
+++ myapp/views.py
@@ -17,7 +17,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('insertPost')
+            return redirect('postList')
         else:
             error_message = 'Invalid login credentials'
             return render(request, 'login.html', {'error_message': error_message})
