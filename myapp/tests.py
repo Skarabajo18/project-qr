4b668ab4d32ca0f970f7e7b2685c98e092b8995e
Skarabajo18 <felipesegovia14@gmail.com>
2023-04-02 23:46:33 -0400

    test

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from myapp.models import Post
 
-# Create your tests here.
+
+class PostModelTestCase(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(
+            title='Test post', body='This is a test post.')
+
+    def test_post_creation(self):
+        self.assertEqual(self.post.title, 'Test post')
+        self.assertEqual(self.post.body, 'This is a test post.')
--- myapp/views.py
+++ myapp/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.views.decorators.csrf import csrf_protect
-from django.shortcuts import render, redirect
+
 from .models import *
 from .forms import *
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
